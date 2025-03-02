6115529169309532564a2e941c7cc319bfcf9c33
Benjamin Crozat <hello@benjamincrozat.com>
2025-03-02 12:28:42 +0100

    Added footer

--- resources/views/components/app.blade.php
+++ resources/views/components/app.blade.php
@@ -47,8 +47,17 @@
             {{ $slot }}
         </main>
 
-        <footer class="mt-8">
-        </footer>
+        <div class="mt-8 bg-gray-100 md:mt-16">
+            <footer class="container py-8">
+                <nav class="flex items-center justify-center gap-8">
+                    <a wire:navigate href="{{ route('home') }}" class="font-medium">Home</a>
+                    <a wire:navigate href="{{ route('posts.index') }}" class="font-medium">Latest</a>
+                    <a href="{{ route('home') }}#about" class="font-medium">About</a>
+                </nav>
+
+                <p class="mt-6 text-center text-gray-400">Please don't steal my content. © {{ date('Y') }} blah blah blah.</p>
+            </footer>
+        </div>
 
         @livewireScriptConfig
 
