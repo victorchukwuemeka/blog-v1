cd3922412e3c83982946d45dfce72b0fd9066148
Benjamin Crozat <hello@benjamincrozat.com>
2025-03-02 12:41:15 +0100

    Tweaked logo

--- resources/views/components/nav.blade.php
+++ resources/views/components/nav.blade.php
@@ -2,9 +2,14 @@
     <a
         wire:navigate
         href="{{ route('home') }}"
+        class="flex items-center gap-3"
     >
         <div class="grid bg-black place-items-center size-9 md:size-10 rounded-[.65rem] md:rounded-xl">
             <x-heroicon-s-bold class="text-white size-5 md:size-6" />
+        </div>
+
+        <div class="font-medium tracking-tighter sr-only sm:not-sr-only sm:text-lg md:text-xl">
+            benjamincrozat.com
         </div>
     </a>
 
