a387e6584d943b8fbee153ef2a2d975357ccbbf7
Benjamin Crozat <hello@benjamincrozat.com>
2025-03-04 17:22:13 +0100

    Added some comments

--- app/Http/Controllers/ShowPostController.php
+++ app/Http/Controllers/ShowPostController.php
@@ -13,8 +13,10 @@ class ShowPostController extends Controller
 
         abort_if(! file_exists($filepath), 404);
 
+        // Let's see when's the last time the file was modified.
         $timestamp = filemtime($filepath);
 
+        // We use the timestamp in the cache key to bust the cache if the file has been modified.
         $cacheKey = "post_{$slug}_$timestamp";
 
         $post = cache()->rememberForever(
