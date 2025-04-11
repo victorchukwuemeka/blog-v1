b98d351f6a93ae9251794e7fcad1be8c03a88329
Benjamin Crozat <hello@benjamincrozat.com>
2025-04-11 14:08:15 +0200

    This messes up with caching in ListPostsController and I have no clue why

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -29,7 +29,7 @@ class AppServiceProvider extends ServiceProvider
         Livewire::component('first-step', FirstStep::class);
         Livewire::component('second-step', SecondStep::class);
 
-        Model::automaticallyEagerLoadRelationships();
+        // Model::automaticallyEagerLoadRelationships();
 
         // This one helps you catch lots of issues. Check
         // the source code to see what it does.
