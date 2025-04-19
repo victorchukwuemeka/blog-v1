e1e65365f7798ef967f7678531c06cfdedf39d62
Benjamin Crozat <hello@benjamincrozat.com>
2025-04-19 18:33:38 +0200

    Added a sitemap generator

---
+++ app/Console/Commands/GenerateSitemap.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Post;
+use Spatie\Sitemap\Sitemap;
+use Illuminate\Console\Command;
+
+class GenerateSitemap extends Command
+{
+    /**
+     * @var string
+     */
+    protected $signature = 'app:generate-sitemap';
+
+    /**
+     * @var string
+     */
+    protected $description = 'Generate the sitemap.';
+
+    public function handle() : void
+    {
+        $sitemap = Sitemap::create(config('app.url'));
+
+        $sitemap->add(route('home'));
+
+        $sitemap->add(route('posts.index'));
+
+        Post::query()
+            ->published()
+            ->cursor()
+            ->each(fn (Post $post) => $sitemap->add(route('posts.show', $post)));
+
+        $sitemap->add(route('links.index'));
+
+        $sitemap->writeToFile($path = public_path('sitemap.xml'));
+
+        $this->info("Sitemap generated successfully at $path");
+    }
+}
