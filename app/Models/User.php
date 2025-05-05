d704b01a92f317f7abc0a53fc6ed63b00479dcfb
Benjamin Crozat <hello@benjamincrozat.com>
2025-05-05 08:57:27 +0200

    Added panel access authorization logic

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,13 +2,15 @@
 
 namespace App\Models;
 
+use Filament\Panel;
 use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Notifications\Notifiable;
+use Filament\Models\Contracts\FilamentUser;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, MustVerifyEmail, Notifiable;
@@ -40,5 +42,10 @@ class User extends Authenticatable
     public function links() : HasMany
     {
         return $this->hasMany(Link::class);
+    }
+
+    public function canAccessPanel(Panel $panel) : bool
+    {
+        return 'hello@benjamincrozat.com' === $this->email;
     }
 }
