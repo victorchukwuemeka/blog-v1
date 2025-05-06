20cca619de65c0619566baa469b5fdc7fadf62e9
Benjamin Crozat <hello@benjamincrozat.com>
2025-05-06 17:25:15 +0200

    Whoops, wrong class names

--- resources/views/components/dropdown/divider.blade.php
+++ resources/views/components/dropdown/divider.blade.php
@@ -1,4 +1,4 @@
-<div {{ $attributes->class('not:first:h-px not:first:my-2 not:first:bg-black/10') }}></div>
+<div {{ $attributes->class('not-first:h-px not-first:my-2 not-first:bg-black/10') }}></div>
 
 @if ($slot->hasActualContent())
     <div class="px-4 py-2 text-xs font-bold tracking-widest uppercase">
