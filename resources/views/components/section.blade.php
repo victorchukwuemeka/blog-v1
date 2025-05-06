2eddc45cf22d1217680a17512baa6d6ec1d530f0
Benjamin Crozat <hello@benjamincrozat.com>
2025-05-06 15:00:24 +0200

    UI fix

--- resources/views/components/section.blade.php
+++ resources/views/components/section.blade.php
@@ -1,6 +1,6 @@
 <section {{ $attributes->class('container scroll-mt-4') }}>
     @if (! empty($title))
-        <h1 class="font-bold tracking-widest text-center text-black uppercase">
+        <h1 class="font-bold tracking-widest text-center text-black uppercase text-balance">
             {{ $title }}
         </h1>
     @endif
