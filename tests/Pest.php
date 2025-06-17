c7a2120644d2bb8ae1e6bbd87d4f80c60d25309e
Benjamin Crozat <hello@benjamincrozat.com>
2025-06-17 09:52:55 +0200

    Switched to withoutDefer()

--- tests/Pest.php
+++ tests/Pest.php
@@ -4,6 +4,7 @@ use Tests\TestCase;
 use Illuminate\Support\Facades\Http;
 
 use function Pest\Laravel\withoutVite;
+use function Pest\Laravel\withoutDefer;
 
 use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
 
@@ -11,12 +12,12 @@ pest()
     ->extend(TestCase::class)
     ->use(LazilyRefreshDatabase::class)
     ->beforeEach(function () {
+        withoutDefer();
+
         // Useful when running tests without Vite running.
         withoutVite();
 
         // Make sure our tests don't make any unwanted HTTP requests.
         Http::preventStrayRequests();
-
-        $this->withoutDefer();
     })
     ->in('Feature');
