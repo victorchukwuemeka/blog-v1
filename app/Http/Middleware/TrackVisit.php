d148b8eaf999a97e811f34ef8bc48262e646c8f1
Benjamin Crozat <hello@benjamincrozat.com>
2025-06-17 09:53:15 +0200

    Switched to a terminatable middlware

--- app/Http/Middleware/TrackVisit.php
+++ app/Http/Middleware/TrackVisit.php
@@ -9,30 +9,41 @@ class TrackVisit
 {
     public function handle(Request $request, \Closure $next, string ...$guards) : Response
     {
-        // Make sure this is a valid visit to track.
-        if ('production' === config('app.env') &&
+        return $next($request);
+    }
+
+    public function terminate(Request $request, Response $response) : void
+    {
+        if ($this->shouldTrack($request)) {
+            // These are needed for Pirsch's API so we need to make sure the values are not empty or null.
+            if (! empty($url = $request->url()) &&
+                ($ip = $request->ip()) &&
+                ($userAgent = $request->userAgent())) {
+                app(\App\Actions\TrackVisit::class)->track(
+                    $url,
+                    $ip,
+                    $userAgent,
+                    $request->header('Accept-Language', ''),
+                    $request->header('Referer', ''),
+                );
+            }
+        }
+    }
+
+    /**
+     * Determine if the request should be tracked.
+     *
+     * Only track visits that meet all of the following:
+     * 1. The app is running in production.
+     * 2. The request is not from Livewire.
+     * 3. The request does not expect a JSON response.
+     * 4. The request uses the GET method.
+     */
+    protected function shouldTrack(Request $request) : bool
+    {
+        return 'production' === config('app.env') &&
             ! $request->hasHeader('X-Livewire') &&
             ! $request->wantsJson() &&
-            'GET' === $request->method()) {
-            defer(function () use ($request) {
-                // These are needed for Pirsch's API so we
-                // need to make sure they are available.
-                if (
-                    ($url = $request->url()) &&
-                    ($ip = $request->ip()) &&
-                    ($userAgent = $request->userAgent())
-                ) {
-                    app(\App\Actions\TrackVisit::class)->track(
-                        $url,
-                        $ip,
-                        $userAgent,
-                        $request->header('Accept-Language', ''),
-                        $request->header('Referer', ''),
-                    );
-                }
-            });
-        }
-
-        return $next($request);
+            'GET' === $request->method();
     }
 }
