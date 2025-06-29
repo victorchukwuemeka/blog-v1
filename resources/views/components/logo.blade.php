8b2c2976a90f72314fd719b971eb9d51b0094ad0
Benjamin Crozat <hello@benjamincrozat.com>
2025-06-29 19:15:38 +0200

    Improved logo

--- resources/views/components/logo.blade.php
+++ resources/views/components/logo.blade.php
@@ -1,3 +1,7 @@
-<div {{ $attributes->class('grid bg-black place-items-center size-9 md:size-10 rounded-[.65rem] md:rounded-xl') }}>
-    <x-heroicon-s-bold class="text-white size-5 md:size-6" />
+<div class="flex gap-4 items-center">
+    <div {{ $attributes->class('grid bg-black place-items-center size-9 md:size-10 rounded-[.65rem] md:rounded-xl') }}>
+        <x-heroicon-s-bold class="text-white size-5 md:size-6" />
+    </div>
+
+    <div class="hidden text-base font-bold tracking-widest text-black uppercase md:block">benjamincrozat.com</div>
 </div>
