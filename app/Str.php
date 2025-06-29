8af77dcef8f7181dcfdb560b4d02ab9f4b80baaa
Benjamin Crozat <hello@benjamincrozat.com>
2025-06-29 21:21:05 +0200

    Added one more type

--- app/Str.php
+++ app/Str.php
@@ -9,6 +9,7 @@ use League\CommonMark\GithubFlavoredMarkdownConverter;
 use Tempest\Highlight\CommonMark\InlineCodeBlockRenderer;
 use League\CommonMark\Node\Inline\AbstractStringContainer;
 use League\CommonMark\Extension\CommonMark\Node\Inline\Code;
+use League\CommonMark\Extension\CommonMark\Node\Inline\Link;
 use League\CommonMark\Extension\CommonMark\Node\Block\Heading;
 use League\CommonMark\Extension\SmartPunct\SmartPunctExtension;
 use League\CommonMark\Extension\CommonMark\Node\Block\FencedCode;
@@ -68,7 +69,7 @@ class Str extends \Illuminate\Support\Str
     protected static function childrenToText(Node $node) : string
     {
         return implode('', array_map(function (AbstractStringContainer|Text $child) {
-            if ($child instanceof AbstractStringContainer || $child instanceof Text) {
+            if ($child instanceof AbstractStringContainer || $child instanceof Link || $child instanceof Text) {
                 return $child->getLiteral();
             }
 
