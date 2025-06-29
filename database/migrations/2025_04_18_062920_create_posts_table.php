4531be950c607276fb7a625ba7c10ada9db626cb
Benjamin Crozat <hello@benjamincrozat.com>
2025-06-29 16:13:35 +0200

    WIP

--- database/migrations/2025_04_18_062920_create_posts_table.php
+++ database/migrations/2025_04_18_062920_create_posts_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->dateTime('published_at')->nullable();
             $table->dateTime('modified_at')->nullable();
             $table->json('recommendations')->nullable();
-            $table->integer('sessions_count')->default(0);
+            $table->unsignedBigInteger('sessions_count')->default(0);
             $table->timestamps();
         });
     }
