4dbdcbfa0ebad21d1248a1b49ed7cbea69cae6fb
Benjamin Crozat <hello@benjamincrozat.com>
2025-06-30 01:42:48 +0200

    WIP

--- app/Filament/Forms/Components/MarkdownEditor.php
+++ app/Filament/Forms/Components/MarkdownEditor.php
@@ -2,25 +2,12 @@
 
 namespace App\Filament\Forms\Components;
 
-use Spatie\Image\Image;
-use Spatie\Image\Enums\Fit;
 use Livewire\Features\SupportFileUploads\TemporaryUploadedFile;
 
 class MarkdownEditor extends \Filament\Forms\Components\MarkdownEditor
 {
     public function saveUploadedFileAttachment(TemporaryUploadedFile $attachment) : ?string
     {
-        $image = Image::load($attachment->temporaryUrl());
-
-        if ($image->getWidth() > 1500 || $image->getHeight() > 1500) {
-            $image->fit(Fit::Contain, 1500, 1500);
-        }
-
-        $image
-            ->quality(70)
-            ->optimize()
-            ->save($attachment->path());
-
         return parent::saveUploadedFileAttachment($attachment);
     }
 }
