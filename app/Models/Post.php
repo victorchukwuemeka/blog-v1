acb79e37f088b3d7889083f8e2f264bd043f0511
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-03 08:19:51 +0200

    Improved Atom feed

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -117,12 +117,19 @@ class Post extends Model implements Feedable
 
     public function toFeedItem() : FeedItem
     {
+        $link = route('posts.show', $this);
+
         return FeedItem::create()
             ->id($this->slug)
             ->title($this->title)
-            ->summary(Str::markdown($this->description ?? ''))
+            ->summary(Str::markdown($this->description . <<<MARKDOWN
+
+[Read more →]($link)
+
+If you like my feed, follow me on [X](https://x.com/benjamincrozat), [LinkedIn](https://www.linkedin.com/in/benjamincrozat/), and [GitHub](https://github.com/benjamincrozat).
+MARKDOWN ?? ''))
             ->updated($this->modified_at ?? $this->published_at)
-            ->link(route('posts.show', $this))
+            ->link($link)
             ->authorName($this->user->name);
     }
 }
