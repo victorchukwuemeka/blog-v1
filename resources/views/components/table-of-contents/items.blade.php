cc08de3a72080a2b2f4d7d9db50853f95560794c
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-08 09:31:55 +0200

    WIP

--- resources/views/components/table-of-contents/items.blade.php
+++ resources/views/components/table-of-contents/items.blade.php
@@ -3,7 +3,7 @@
 <ul {{ $attributes->class('ml-4 grid gap-1') }}>
     @foreach ($headings as $heading)
         <li>
-            <a href="#{{ $heading['slug'] }}" class="font-medium group line-clamp-1">
+            <a href="#{{ $heading['slug'] }}" class="font-medium group">
                 <span class="opacity-50 group-hover:text-blue-600">→</span>
                 <span class="ml-1 underline transition-colors group-hover:text-blue-600 underline-offset-4 decoration-1 decoration-black/30 group-hover:decoration-blue-600/50">{{ $heading['text'] }}</span>
             </a>
