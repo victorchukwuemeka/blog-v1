c722220c523d12532986444d87f93d95f97741ca
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-09 10:27:19 +0200

    No need for that

--- app/Models/Link.php
+++ app/Models/Link.php
@@ -78,17 +78,6 @@ class Link extends Model
         return $this;
     }
 
-    public function approveWithoutNotifying(?string $notes = null) : self
-    {
-        $this->update([
-            'notes' => $notes,
-            'is_approved' => now(),
-            'is_declined' => null,
-        ]);
-
-        return $this;
-    }
-
     public function decline() : self
     {
         $this->update([
