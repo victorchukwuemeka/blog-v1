6e002868a5d4c68232bb8601cba9a5154a8c9fcc
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-13 01:49:41 +0200

    Fixed breaking tests

--- tests/Feature/App/Http/Controllers/HomeControllerTest.php
+++ tests/Feature/App/Http/Controllers/HomeControllerTest.php
@@ -1,24 +1,39 @@
 <?php
 
+use App\Models\Link;
 use App\Models\Post;
+use App\Models\User;
 
 use function Pest\Laravel\get;
 
 use Illuminate\Support\Collection;
 
-it('renders with popular and latest posts', function () {
+it("renders with popular and latest posts, links, and the creator's about section", function () {
     Post::factory(15)->create(['sessions_count' => 0]);
+
     Post::factory(15)->create(['sessions_count' => random_int(1, 1000)]);
+
+    Link::factory(15)->approved()->create();
+
+    User::factory()->create([
+        'github_login' => 'benjamincrozat',
+    ]);
 
     get(route('home'))
         ->assertOk()
         ->assertViewIs('home')
         ->assertViewHas('popular', fn (Collection $popular) => 12 === $popular->count())
-        ->assertViewHas('latest', fn (Collection $latest) => 12 === $latest->count());
+        ->assertViewHas('latest', fn (Collection $latest) => 12 === $latest->count())
+        ->assertViewHas('links', fn (Collection $links) => 12 === $links->count())
+        ->assertViewHas('aboutUser', fn (User $aboutUser) => 'benjamincrozat' === $aboutUser->github_login);
 });
 
 it('does not show popular posts if there are no sessions', function () {
     Post::factory(15)->create(['sessions_count' => 0]);
+
+    User::factory()->create([
+        'github_login' => 'benjamincrozat',
+    ]);
 
     get(route('home'))
         ->assertViewHas('popular', fn (Collection $popular) => $popular->isEmpty());
