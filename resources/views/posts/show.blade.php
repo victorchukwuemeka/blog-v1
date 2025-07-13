d7595fba3e146ca4fa3bfa0fd3df6e77a0e8f4b0
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-13 20:03:06 +0200

    WIP

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -14,10 +14,14 @@
         ])>
             <article>
                 @if ($post->hasImage())
-                    <img src="{{ $post->image_url }}" alt="{{ $post->title  }}" class="object-cover mb-12 w-full rounded-xl ring-1 shadow-xl md:mb-16 ring-black/5 aspect-video" />
+                    <img
+                        src="{{ $post->image_url }}"
+                        alt="{{ $post->title  }}"
+                        class="object-cover mb-12 w-full rounded-xl ring-1 shadow-xl md:mb-16 ring-black/5 aspect-video"
+                    />
                 @endif
 
-                <p class="text-sm font-normal tracking-widest text-center uppercase">
+                <p class="text-sm font-normal tracking-widest text-center uppercase md:text-base">
                     {{ trans_choice(':count minute|:count minutes', $post->read_time) }}
                     read
                 </p>
@@ -26,139 +30,137 @@
                     {{ $post->title }}
                 </h1>
 
-                <div class="mt-12 md:mt-16">
-                    <div class="grid grid-cols-2 gap-4 text-sm leading-tight md:grid-flow-col md:grid-cols-none md:auto-cols-fr">
-                        <div class="p-3 text-center bg-gray-50 rounded-lg">
-                            <x-heroicon-o-calendar class="mx-auto mb-2 opacity-75 size-6" />
+                <div class="grid grid-cols-2 gap-4 mt-12 text-sm leading-tight md:mt-16 md:grid-flow-col md:grid-cols-none md:auto-cols-fr">
+                    <div class="p-3 text-center bg-gray-50 rounded-lg">
+                        <x-heroicon-o-calendar class="mx-auto mb-2 opacity-75 size-6" />
 
-                            @if ($post->modified_at)
-                                Modified
-                            @elseif ($post->published_at)
-                                Published
-                            @else
-                                Drafted
-                            @endif
-
-                            <br />
-
-                            {{ ($post->modified_at ?? $post->published_at ?? $post->created_at)->isoFormat('ll') }}
-                        </div>
-
-                        <a
-                            wire:navigate
-                            href="{{ route('authors.show', $post->user) }}"
-                        >
-                            <div class="p-3 text-center bg-gray-50 rounded-lg transition-colors hover:bg-blue-50 hover:text-blue-900">
-                                <img src="{{ $post->user->avatar }}" class="mx-auto mb-2 rounded-full size-6" />
-                                Written by<br />
-                                {{ $post->user->name }}
-                            </div>
-                        </a>
-
-                        @if (! $post->is_commercial)
-                            <a href="#comments">
-                                <div @class([
-                                    'flex-1 p-3 text-center bg-gray-50 rounded-lg transition-colors hover:bg-blue-50 hover:text-blue-900',
-                                    'text-blue-600 bg-blue-50!' => $post->comments_count,
-                                ])>
-                                    <x-heroicon-o-chat-bubble-oval-left-ellipsis class="mx-auto mb-2 opacity-75 size-6" />
-                                    {{ $post->comments_count }}<br />
-                                    {{ trans_choice('comment|comments', $post->comments_count) }}
-                                </div>
-                            </a>
+                        @if ($post->modified_at)
+                            Modified
+                        @elseif ($post->published_at)
+                            Published
+                        @else
+                            Drafted
                         @endif
 
-                        <x-dropdown>
-                            <x-slot:btn
-                                data-pirsch-event='Clicked "Actions"'
-                                class="p-3 w-full h-full text-center bg-gray-50 rounded-lg transition-colors hover:bg-blue-50 hover:text-blue-900"
-                            >
-                                <x-heroicon-o-ellipsis-horizontal
-                                    class="mx-auto transition-transform size-6 md:size-7"
-                                    x-bind:class="{ 'rotate-90': open }"
-                                />
-                                Actions
-                            </x-slot>
+                        <br />
 
-                            <x-slot:items>
-                                @if ($user?->isAdmin())
-                                    <x-dropdown.divider>
-                                        Admin
-                                    </x-dropdown.divider>
+                        {{ ($post->modified_at ?? $post->published_at ?? $post->created_at)->isoFormat('ll') }}
+                    </div>
 
-                                    <x-dropdown.item
-                                        href="{{ route('filament.admin.resources.posts.edit', $post) }}"
-                                        data-pirsch-event='Clicked "Edit article"'
-                                    >
-                                        <x-heroicon-o-pencil-square class="size-4" />
-                                        Edit article
-                                    </x-dropdown.item>
-                                @endif
+                    <a
+                        wire:navigate
+                        href="{{ route('authors.show', $post->user) }}"
+                    >
+                        <div class="p-3 text-center bg-gray-50 rounded-lg transition-colors hover:bg-blue-50 hover:text-blue-900">
+                            <img src="{{ $post->user->avatar }}" class="mx-auto mb-2 rounded-full size-6" />
+                            Written by<br />
+                            {{ $post->user->name }}
+                        </div>
+                    </a>
 
+                    @if (! $post->is_commercial)
+                        <a href="#comments">
+                            <div @class([
+                                'flex-1 p-3 text-center bg-gray-50 rounded-lg transition-colors hover:bg-blue-50 hover:text-blue-900',
+                                'text-blue-600 bg-blue-50!' => $post->comments_count,
+                            ])>
+                                <x-heroicon-o-chat-bubble-oval-left-ellipsis class="mx-auto mb-2 opacity-75 size-6" />
+                                {{ $post->comments_count }}<br />
+                                {{ trans_choice('comment|comments', $post->comments_count) }}
+                            </div>
+                        </a>
+                    @endif
+
+                    <x-dropdown>
+                        <x-slot:btn
+                            data-pirsch-event='Clicked "Actions"'
+                            class="p-3 w-full h-full text-center bg-gray-50 rounded-lg transition-colors hover:bg-blue-50 hover:text-blue-900"
+                        >
+                            <x-heroicon-o-ellipsis-horizontal
+                                class="mx-auto transition-transform size-6 md:size-7"
+                                x-bind:class="{ 'rotate-90': open }"
+                            />
+                            Actions
+                        </x-slot>
+
+                        <x-slot:items>
+                            @if ($user?->isAdmin())
                                 <x-dropdown.divider>
-                                    Chat
+                                    Admin
                                 </x-dropdown.divider>
 
                                 <x-dropdown.item
-                                    :href="'https://chatgpt.com/?q=' . urlencode($post->toPrompt())"
-                                    target="_blank"
-                                    data-pirsch-event='Clicked "Ask ChatGPT"'
+                                    href="{{ route('filament.admin.resources.posts.edit', $post) }}"
+                                    data-pirsch-event='Clicked "Edit article"'
                                 >
-                                    <x-icon-openai class="size-4" />
-                                    Ask ChatGPT
+                                    <x-heroicon-o-pencil-square class="size-4" />
+                                    Edit article
                                 </x-dropdown.item>
+                            @endif
 
-                                <x-dropdown.item
-                                    :href="'https://claude.ai/new?q=' . urlencode($post->toPrompt())"
-                                    target="_blank"
-                                    data-pirsch-event='Clicked "Ask Claude"'
-                                >
-                                    <x-icon-claude class="size-4" />
-                                    Ask Claude
-                                </x-dropdown.item>
-                            </x-slot>
-                        </x-dropdown>
-                    </div>
+                            <x-dropdown.divider>
+                                Chat
+                            </x-dropdown.divider>
 
-                    @if (! empty($headings = extract_headings_from_markdown($post->content)))
-                        <x-table-of-contents
-                            :$headings
-                            class="mt-4 ml-0"
-                        />
+                            <x-dropdown.item
+                                :href="'https://chatgpt.com/?q=' . urlencode($post->toPrompt())"
+                                target="_blank"
+                                data-pirsch-event='Clicked "Ask ChatGPT"'
+                            >
+                                <x-icon-openai class="size-4" />
+                                Ask ChatGPT
+                            </x-dropdown.item>
+
+                            <x-dropdown.item
+                                :href="'https://claude.ai/new?q=' . urlencode($post->toPrompt())"
+                                target="_blank"
+                                data-pirsch-event='Clicked "Ask Claude"'
+                            >
+                                <x-icon-claude class="size-4" />
+                                Ask Claude
+                            </x-dropdown.item>
+                        </x-slot>
+                    </x-dropdown>
+                </div>
+
+                @if (! empty($headings = extract_headings_from_markdown($post->content)))
+                    <x-table-of-contents
+                        :$headings
+                        class="mt-4 ml-0"
+                    />
+                @endif
+
+                <x-prose class="mt-8">
+                    {!! $post->formatted_content !!}
+
+                    @if ($post->link)
+                        <p>
+                            <a href="{{ $post->link->url }}" target="_blank">
+                                Read more on {{ $post->link->domain }} →
+                            </a>
+                        </p>
                     @endif
 
-                    <x-prose class="mt-8">
-                        {!! $post->formatted_content !!}
+                    @if (! empty($post->recommendedPosts) && ! $post->is_commercial)
+                        <hr />
 
-                        @if ($post->link)
-                            <p>
-                                <a href="{{ $post->link->url }}" target="_blank">
-                                    Read more on {{ $post->link->domain }} →
-                                </a>
-                            </p>
-                        @endif
+                        <p>Did you like this article? Then, keep learning:</p>
 
-                        @if (! empty($post->recommendedPosts) && ! $post->is_commercial)
-                            <hr />
-
-                            <p>Did you like this article? Then, keep learning:</p>
-
-                            <ul>
-                                @foreach ($post->recommendedPosts as $recommendedPost)
-                                    <li>
-                                        <a
-                                            wire:navigate
-                                            href="{{ route('posts.show', $recommendedPost) }}"
-                                            data-pirsch-event='Clicked on recommended post "{{ $recommendedPost->title }}"'
-                                        >
-                                            {{ trim($recommendedPost->reason, '.') }}
-                                        </a>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        @endif
-                    </x-prose>
-                </div>
+                        <ul>
+                            @foreach ($post->recommendedPosts as $recommendedPost)
+                                <li>
+                                    <a
+                                        wire:navigate
+                                        href="{{ route('posts.show', $recommendedPost) }}"
+                                        data-pirsch-event='Clicked on recommended post "{{ $recommendedPost->title }}"'
+                                    >
+                                        {{ trim($recommendedPost->reason, '.') }}
+                                    </a>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </x-prose>
             </article>
 
             @if (! $post->is_commercial)
