9b392b51928274acaa4bdda44df6620f3cb64ec2
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-15 13:00:13 +0200

    WIP

--- resources/views/components/comment.blade.php
+++ resources/views/components/comment.blade.php
@@ -20,10 +20,20 @@
                     </span>
                 </div>
 
-                <div>
+                <div class="flex gap-2 items-center">
+                    @if ($user?->isAdmin())
+                        <a
+                            href="{{ route('filament.admin.resources.comments.edit', $comment) }}"
+                            class="grid place-items-center bg-gray-100 rounded-full transition-colors hover:bg-gray-200 size-8"
+                        >
+                            <x-heroicon-o-pencil class="size-4" />
+                            <span class="sr-only">Edit</span>
+                        </a>
+                    @endcan
+
                     @can('delete', $comment)
                         <button
-                            class="font-medium text-red-600"
+                            class="grid place-items-center text-red-600 bg-red-50 rounded-full transition-colors hover:bg-red-100 size-8"
                             wire:click="delete({{ $comment->id }})"
                             wire:confirm="Are you sure you want to delete this comment?"
                             wire:loading.attr="disabled"
