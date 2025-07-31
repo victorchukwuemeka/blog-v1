3fae3d7fe5ce349bd8dd1501fdc85ec63fccb968
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-31 12:59:46 +0200

    WIP

--- resources/views/vendor/pagination/tailwind.blade.php
+++ resources/views/vendor/pagination/tailwind.blade.php
@@ -1,5 +1,5 @@
 @if ($paginator->hasPages())
-    <nav role="navigation" aria-label="Pagination Navigation" class="flex flex-wrap items-center justify-center gap-4 md:flex-nowrap md:justify-between">
+    <nav role="navigation" aria-label="Pagination Navigation" class="flex flex-wrap gap-4 justify-center items-center md:flex-nowrap md:justify-between">
         <div class="text-gray-500">
             Showing
             @if ($paginator->firstItem())
@@ -14,22 +14,29 @@
             results
         </div>
 
-        <div class="flex flex-wrap items-center gap-1">
+        <div class="flex flex-wrap gap-1 items-center">
             @if ($paginator->onFirstPage())
-                <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}" class="grid text-gray-300 rounded-lg bg-gray-50 size-8 place-items-center">
+                <span aria-disabled="true" aria-label="{{ __('pagination.previous') }}" class="grid place-items-center text-gray-300 bg-gray-50 rounded-lg size-8">
                     <span aria-hidden="true">
                         ←
                     </span>
                 </span>
             @else
-                <a wire:navigate href="{{ $paginator->previousPageUrl() }}" rel="prev" aria-label="{{ __('pagination.previous') }}" class="grid transition-colors rounded-lg bg-gray-50 hover:bg-gray-100 size-8 place-items-center">
+                <a
+                    wire:navigate
+                    href="{{ $paginator->previousPageUrl() }}"
+                    rel="prev"
+                    aria-label="{{ __('pagination.previous') }}"
+                    data-pirsch-event="Clicked pagination previous"
+                    class="grid place-items-center bg-gray-50 rounded-lg transition-colors hover:bg-gray-100 size-8"
+                >
                     ←
                 </a>
             @endif
 
             @foreach ($elements as $element)
                 @if (is_string($element))
-                    <span aria-disabled="true" class="grid text-gray-300 rounded-lg bg-gray-50 size-8 place-items-center">
+                    <span aria-disabled="true" class="grid place-items-center text-gray-300 bg-gray-50 rounded-lg size-8">
                         <span>{{ $element }}</span>
                     </span>
                 @endif
@@ -38,11 +45,18 @@
                 @if (is_array($element))
                     @foreach ($element as $page => $url)
                         @if ($page == $paginator->currentPage())
-                            <span aria-current="page" class="grid text-white bg-gray-900 rounded-lg size-8 place-items-center">
+                            <span aria-current="page" class="grid place-items-center text-white bg-gray-900 rounded-lg size-8">
                                 <span>{{ $page }}</span>
                             </span>
                         @else
-                            <a wire:navigate href="{{ $url }}" aria-label="{{ __('Go to page :page', compact('page')) }}" class="grid transition-colors rounded-lg bg-gray-50 hover:bg-gray-100 size-8 place-items-center">
+                            <a
+                                wire:navigate
+                                href="{{ $url }}"
+                                aria-label="{{ __('Go to page :page', compact('page')) }}"
+                                data-pirsch-event="Clicked pagination page"
+                                data-pirsch-meta-page="{{ $page }}"
+                                class="grid place-items-center bg-gray-50 rounded-lg transition-colors hover:bg-gray-100 size-8"
+                            >
                                 {{ $page }}
                             </a>
                         @endif
@@ -51,11 +65,18 @@
             @endforeach
 
             @if ($paginator->hasMorePages())
-                <a wire:navigate href="{{ $paginator->nextPageUrl() }}" rel="next" aria-label="{{ __('pagination.next') }}" class="grid transition-colors rounded-lg bg-gray-50 hover:bg-gray-100 size-8 place-items-center">
+                <a
+                    wire:navigate
+                    href="{{ $paginator->nextPageUrl() }}"
+                    rel="next"
+                    aria-label="{{ __('pagination.next') }}"
+                    data-pirsch-event="Clicked pagination next"
+                    class="grid place-items-center bg-gray-50 rounded-lg transition-colors hover:bg-gray-100 size-8"
+                >
                     →
                 </a>
             @else
-                <span aria-disabled="true" aria-label="{{ __('pagination.next') }}" class="grid text-gray-300 rounded-lg bg-gray-50 size-8 place-items-center">
+                <span aria-disabled="true" aria-label="{{ __('pagination.next') }}" class="grid place-items-center text-gray-300 bg-gray-50 rounded-lg size-8">
                     <span aria-hidden="true">
                         →
                     </span>
