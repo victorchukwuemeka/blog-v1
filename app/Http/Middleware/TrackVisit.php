9793021983ab30bb9a362ddcf0889f6173ac6272
Benjamin Crozat <hello@benjamincrozat.com>
2025-07-31 15:05:35 +0200

    WIP

--- app/Http/Middleware/TrackVisit.php
+++ app/Http/Middleware/TrackVisit.php
@@ -39,43 +39,17 @@ class TrackVisit
      * 1. The app is running in production.
      * 2. The request is not from Livewire.
      * 3. The request does not expect a JSON response.
-     * 4. The request is not a prefetch request.
-     * 5. The request uses the GET method.
-     * 6. The request is not from a crawler. (Pirsch already filters them out, but some may have slipped through.)
-     * 7. The request is not from an admin.
+     * 4. The request uses the GET method.
+     * 5. The request is not from a crawler. (Pirsch already filters them out, but some may have slipped through.)
+     * 6. The request is not from an admin.
      */
     protected function shouldTrack(Request $request) : bool
     {
         return 'production' === config('app.env') &&
             ! $request->hasHeader('X-Livewire') &&
             ! $request->wantsJson() &&
-            ! $this->isPrefetch($request) &&
             'GET' === $request->method() &&
             ! app(CrawlerDetect::class)->isCrawler($request->userAgent()) &&
             ! $request->user()?->isAdmin();
-    }
-
-    protected function isPrefetch(Request $request) : bool
-    {
-        // According to RFC 9218, browsers should send a `Purpose: prefetch` header when prefetching.
-        // Firefox historically used proprietary headers such as `X-Moz: prefetch`.
-        // We consider any of these signals as a prefetch request.
-
-        $purpose = strtolower($request->header('Purpose', ''));
-        if ('prefetch' === $purpose) {
-            return true;
-        }
-
-        $xPurpose = strtolower($request->header('X-Purpose', ''));
-        if ('prefetch' === $xPurpose) {
-            return true;
-        }
-
-        $xMoz = strtolower($request->header('X-Moz', ''));
-        if ('prefetch' === $xMoz) {
-            return true;
-        }
-
-        return false;
     }
 }
