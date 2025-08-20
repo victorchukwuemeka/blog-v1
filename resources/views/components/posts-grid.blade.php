47e53189d21b4012156275c8e5a06e065a72db75
Benjamin Crozat <hello@benjamincrozat.com>
2025-08-20 14:56:39 +0200

    Revert "WIP"
    This reverts commit 0999e468bf550b747b83df76dcaab39bffc7499b.

--- resources/views/components/posts-grid.blade.php
+++ resources/views/components/posts-grid.blade.php
@@ -1,8 +1,6 @@
 @props(['posts'])
 
 <ul {{ $attributes->class('grid gap-10 gap-y-16 xl:gap-x-16 md:grid-cols-2 xl:grid-cols-3') }}>
-    {{ $slot }}
-
     @foreach ($posts as $post)
         <li>
             <x-post :$post />
