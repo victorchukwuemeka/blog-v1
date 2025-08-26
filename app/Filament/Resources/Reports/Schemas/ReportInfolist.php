5ea450209313131d5ada050da88d58d3d6988077
Benjamin Crozat <hello@benjamincrozat.com>
2025-08-26 17:35:15 +0200

    WIP

--- app/Filament/Resources/Reports/Schemas/ReportInfolist.php
+++ app/Filament/Resources/Reports/Schemas/ReportInfolist.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\Reports\Schemas;
 use Filament\Schemas\Schema;
 use Filament\Schemas\Components\Grid;
 use Filament\Infolists\Components\TextEntry;
+use Filament\Forms\Components\MarkdownEditor;
 
 class ReportInfolist
 {
@@ -16,7 +17,7 @@ class ReportInfolist
                     ->label('Post')
                     ->columnSpanFull(),
 
-                TextEntry::make('content')
+                MarkdownEditor::make('content')
                     ->columnSpan(2),
 
                 Grid::make()
