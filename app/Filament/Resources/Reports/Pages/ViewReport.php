7bdd0e68e9ea4dd0275b531811e823f50e1e991b
Benjamin Crozat <hello@benjamincrozat.com>
2025-08-26 17:44:07 +0200

    WIP

--- app/Filament/Resources/Reports/Pages/ViewReport.php
+++ app/Filament/Resources/Reports/Pages/ViewReport.php
@@ -2,18 +2,23 @@
 
 namespace App\Filament\Resources\Reports\Pages;
 
-use App\Filament\Resources\Reports\ReportResource;
-use Filament\Actions\EditAction;
+use App\Models\Report;
+use Illuminate\Support\Js;
+use Filament\Actions\Action;
 use Filament\Resources\Pages\ViewRecord;
+use App\Filament\Resources\Reports\ReportResource;
 
 class ViewReport extends ViewRecord
 {
     protected static string $resource = ReportResource::class;
 
-    protected function getHeaderActions(): array
+    protected function getHeaderActions() : array
     {
         return [
-            EditAction::make(),
+            Action::make('copy')
+                ->label('Copy as Markdown')
+                ->icon('heroicon-o-clipboard-document')
+                ->alpineClickHandler(fn (Report $record) => 'window.navigator.clipboard.writeText(' . Js::from($record->content) . ')'),
         ];
     }
 }
