883523358d0e62cdeca5377e740480237f6e280a
Benjamin Crozat <benjamincrozat@me.com>
2025-08-31 17:16:54 +0200

    Add copy buttons to revision infolist

--- app/Filament/Resources/Revisions/Schemas/RevisionInfolist.php
+++ app/Filament/Resources/Revisions/Schemas/RevisionInfolist.php
@@ -16,27 +16,33 @@ class RevisionInfolist
                 Grid::make()
                     ->schema([
                         TextEntry::make('report.post.title')
-                            ->label('Revision for'),
+                            ->label('Revision for')
+                            ->copyable(),
 
                         TextEntry::make('created_at')
                             ->dateTime()
-                            ->label('Creation Date'),
+                            ->label('Creation Date')
+                            ->copyable(),
 
                         TextEntry::make('updated_at')
                             ->dateTime()
-                            ->label('Last Modification Date'),
+                            ->label('Last Modification Date')
+                            ->copyable(),
 
                         TextEntry::make('title')
-                            ->state(fn (Revision $record) => $record->data['title']),
+                            ->state(fn (Revision $record) => $record->data['title'])
+                            ->copyable(),
 
                         TextEntry::make('description')
-                            ->state(fn (Revision $record) => $record->data['description']),
+                            ->state(fn (Revision $record) => $record->data['description'])
+                            ->copyable(),
                     ])
                     ->columnSpanFull(),
 
                 TextEntry::make('content')
                     ->state(fn (Revision $record) => $record->data['content'])
                     ->markdown()
+                    ->copyable()
                     ->columnSpan(2),
             ])
             ->columns(3);
