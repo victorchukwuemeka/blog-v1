e5688650384e17c9c66b83f5ae53768ffe5b71b3
Benjamin Crozat <hello@benjamincrozat.com>
2025-09-03 15:48:51 +0200

    Added Cloudways promo code

--- resources/views/components/deals/cloudways.blade.php
+++ resources/views/components/deals/cloudways.blade.php
@@ -1,9 +1,14 @@
 <x-deals.item
     name="Cloudways"
     headline="Easily deploy PHP web apps"
-    subheadline="PHP 8, scalability, Cloudflare, caching, 24/7 support, and more with Cloudways"
     cta="Start free"
     cta-color="bg-[#3641C2]!"
     href="{{ route('merchants.show', 'cloudways-php') }}"
     :src="Vite::asset('resources/img/screenshots/cloudways.webp')"
-/>
+>
+    <x-slot:subheadline>
+PHP 8, scalability, Cloudflare, caching, 24/7 support, and more with Cloudways.
+
+Get **30% off for 5 months** using *SUMMER305*.
+    </x-slot:subheadline>
+</x-deals.item>
