e33ac70f4ae8a7fa626cdfcc9a513a0bfe2dbf99
Benjamin Crozat <hello@benjamincrozat.com>
2025-09-28 14:48:59 +0200

    Improved category page

--- resources/views/categories/show.blade.php
+++ resources/views/categories/show.blade.php
@@ -8,8 +8,20 @@
             : $category->name"
         :big-title="$posts->currentPage() === 1"
     >
+        @if ($category->content)
+            <x-prose class="mx-auto max-w-(--breakpoint-md)!">
+                {!! $category->content !!}
+            </x-prose>
+        @endif
+
         @if ($posts->isNotEmpty())
-            <ul class="grid gap-10 gap-y-16 xl:gap-x-16 md:grid-cols-2 xl:grid-cols-3">
+            @if ($category->content)
+                <x-heading class="mt-16">
+                    Articles
+                </x-heading>
+            @endif
+
+            <ul class="grid gap-10 gap-y-16 mt-10 xl:gap-x-16 md:grid-cols-2 xl:grid-cols-3">
                 @foreach ($posts as $post)
                     <li>
                         <x-post :$post />
