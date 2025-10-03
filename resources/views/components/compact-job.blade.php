5c656bc76363604afecac29f3bd7a4f98b6cc678
Benjamin Crozat <hello@benjamincrozat.com>
2025-10-03 17:22:51 +0200

    WIP

--- resources/views/components/compact-job.blade.php
+++ resources/views/components/compact-job.blade.php
@@ -1,29 +1,34 @@
 @props(['job'])
 
-<div {{ $attributes }}>
-    <a
-        wire:navigate
-        href="{{ route('job-listings.show', $job->slug) }}"
-        class="font-bold transition-colors hover:text-blue-600"
-        data-pirsch-event="Clicked job"
-        data-pirsch-meta-title="{{ $job->title }}"
-    >
-        {{ $job->title }}
-    </a>
+<div {{ $attributes->class('flex items-center gap-4 md:gap-6') }}>
+    <div class="flex-none size-12 rounded-full ring-1 ring-black/10 grid place-items-center">
+        <x-heroicon-o-building-office class="text-gray-500 size-6" />
+    </div>
 
-    <div class="flex items-center gap-3 mt-1">
-        <p class="flex items-center gap-2">
-            <x-heroicon-o-building-office class="size-4 opacity-75" />
-            {{ $job->company->name }}
-        </p>
+    <div>
+        <a
+            wire:navigate
+            href="{{ route('job-listings.show', $job->slug) }}"
+            class="font-bold transition-colors hover:text-blue-600"
+            data-pirsch-event="Clicked job"
+            data-pirsch-meta-title="{{ $job->title }}"
+        >
+            {{ $job->title }}
+        </a>
 
-        @if ($job->min_salary && $job->max_salary)
-            <div class="text-xs opacity-50">/</div>
-
-            <p class="flex items-center gap-2">
-                <x-heroicon-o-banknotes class="size-4 opacity-75" />
-                {{ Number::currency($job->min_salary, $job->currency ?? 'USD') }}—{{ Number::currency($job->max_salary, $job->currency ?? 'USD') }}
+        <div class="flex items-center gap-3 mt-1">
+            <p>
+                {{ $job->company->name }}
             </p>
-        @endif
+
+            @if ($job->min_salary && $job->max_salary)
+                <div class="text-xs opacity-50">/</div>
+
+                <p class="flex items-center gap-2">
+                    <x-heroicon-o-banknotes class="size-4 opacity-75" />
+                    {{ Number::currency($job->min_salary, $job->currency ?? 'USD') }}—{{ Number::currency($job->max_salary, $job->currency ?? 'USD') }}
+                </p>
+            @endif
+        </div>
     </div>
 </div>
